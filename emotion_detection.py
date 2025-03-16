c76d4b0e2da281cde0647ee6a9a6d78da6df54d7
Roberto Sorto <roberto.sorto@ondev.one>
2025-03-16 16:08:21 -0600

    emotion_detection.py added

---
+++ emotion_detection.py
@@ -0,0 +1,17 @@
+"""
+Emotion detection Library
+"""
+import requests
+import json
+
+def emotion_detection(text_to_analyze):
+  """
+  Querying the Watson API and formating the response
+  """
+  url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+  headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+  obj = { "raw_document": { "text": text_to_analyze } }
+
+  response = requests.post(url, json = obj, headers = headers)
+
+  return response.text
